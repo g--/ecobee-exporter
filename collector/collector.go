44b452bfd92632275a470cee5c573f824c14670d
Geoff Oakham <geoff.oakham@swingtix.ca>
2023-07-07 11:14:34 -0400

    Adding equipment (heat, cool, fan)

--- collector/collector.go
+++ collector/collector.go
@@ -29,7 +29,10 @@ type eCollector struct {
 	actualTemperature, targetTemperatureMin, targetTemperatureMax *prometheus.Desc
 
 	// sensor descriptors
-	temperature, humidity, occupancy, inUse, currentHvacMode *prometheus.Desc
+	temperature, humidity, occupancy, inUse, currentHvacMode, voc, co2, air_quality, air_quality_accuracy, air_pressure *prometheus.Desc
+
+	// equipment
+	equipmentRunning, auxHeat1, auxHeat2, auxHeat3, compCool1, compCool2, heatPump1, heatPump2, fan *prometheus.Desc
 }
 
 // NewEcobeeCollector returns a new eCollector with the given prefix assigned to all
@@ -85,6 +88,31 @@ func NewEcobeeCollector(c *ecobee.Client, metricPrefix string) *eCollector {
 			"occupancy reported by a sensor (0 or 1)",
 			sensor,
 		),
+		voc: d.new(
+			"volitile_organic_compounds_ppm",
+			"VOCs",
+			sensor,
+		),
+		co2: d.new(
+			"carbon_dioxide_ppm",
+			"CO2",
+			sensor,
+		),
+		air_quality: d.new(
+			"air_quality",
+			"air quality",
+			sensor,
+		),
+		air_quality_accuracy: d.new(
+			"air_quality_accuracy",
+			"air quality accuracy",
+			sensor,
+		),
+		air_pressure: d.new(
+			"air_pressure",
+			"air pressure in ??",
+			sensor,
+		),
 		inUse: d.new(
 			"in_use",
 			"is sensor being used in thermostat calculations (0 or 1)",
@@ -94,6 +122,51 @@ func NewEcobeeCollector(c *ecobee.Client, metricPrefix string) *eCollector {
 			"currenthvacmode",
 			"current hvac mode of thermostat",
 			[]string{"thermostat_id", "thermostat_name", "current_hvac_mode"},
+		),
+		equipmentRunning: d.new(
+			"equipment_running",
+			"equipment currently running (1 for on, 0 for off)",
+			append(runtime, "name"),
+		),
+		auxHeat1: d.new(
+			"aux_heat1",
+			"Heat stage 1",
+			runtime,
+		),
+		auxHeat2: d.new(
+			"aux_heat2",
+			"Heat stage 2",
+			runtime,
+		),
+		auxHeat3: d.new(
+			"aux_heat3",
+			"Heat stage 3",
+			runtime,
+		),
+		compCool1: d.new(
+			"comp_cool1",
+			"Cool stage 1",
+			runtime,
+		),
+		compCool2: d.new(
+			"comp_cool2",
+			"Cool stage 2",
+			runtime,
+		),
+		heatPump1: d.new(
+			"heat_pump1",
+			"Heat pump stage 1",
+			runtime,
+		),
+		heatPump2: d.new(
+			"heat_pump2",
+			"Heat pump stage 2",
+			runtime,
+		),
+		fan: d.new(
+			"fan",
+			"current hvac mode of thermostat",
+			runtime,
 		),
 	}
 }
@@ -106,9 +179,34 @@ func (c *eCollector) Describe(ch chan<- *prometheus.Desc) {
 	ch <- c.targetTemperatureMin
 	ch <- c.temperature
 	ch <- c.humidity
+	ch <- c.voc
+	ch <- c.co2
+	ch <- c.air_quality
+	ch <- c.air_quality_accuracy
+	ch <- c.air_pressure
 	ch <- c.occupancy
 	ch <- c.inUse
 	ch <- c.currentHvacMode
+	ch <- c.equipmentRunning
+	ch <- c.auxHeat1
+	ch <- c.auxHeat2
+	ch <- c.auxHeat3
+	ch <- c.compCool1
+	ch <- c.compCool2
+	ch <- c.heatPump1
+	ch <- c.heatPump2
+	ch <- c.fan
+}
+
+func is_thing_running(values []int) float64 {
+	var r = 0.0
+	for _, v := range values {
+		if v > 0 {
+			r = 1.0
+		}
+
+	}
+	return r
 }
 
 // Collect retrieves thermostat data via the ecobee API.
@@ -118,7 +216,9 @@ func (c *eCollector) Collect(ch chan<- prometheus.Metric) {
 		SelectionType:   "registered",
 		IncludeSensors:  true,
 		IncludeRuntime:  true,
+		IncludeExtendedRuntime:  true,
 		IncludeSettings: true,
+		IncludeWeather: true,
 	})
 	elapsed := time.Now().Sub(start)
 	ch <- prometheus.MustNewConstMetric(c.fetchTime, prometheus.GaugeValue, elapsed.Seconds())
@@ -126,6 +226,9 @@ func (c *eCollector) Collect(ch chan<- prometheus.Metric) {
 		log.Error(err)
 		return
 	}
+	// https://developer.ecobee.com/home/developer/api/documentation/v1/objects/Thermostat.shtml
+	// https://developer.ecobee.com/home/developer/api/documentation/v1/objects/Weather.shtml
+	//   t.Weather.forecasts[0].temperature
 	for _, t := range tt {
 		tFields := []string{t.Identifier, t.Name}
 		if t.Runtime.Connected {
@@ -140,6 +243,56 @@ func (c *eCollector) Collect(ch chan<- prometheus.Metric) {
 			)
 			ch <- prometheus.MustNewConstMetric(
 				c.currentHvacMode, prometheus.GaugeValue, 0, t.Identifier, t.Name, t.Settings.HvacMode,
+			)
+			ch <- prometheus.MustNewConstMetric(
+				c.equipmentRunning, prometheus.GaugeValue, is_thing_running(t.ExtendedRuntime.AuxHeat1), append(tFields, "heat 1")...,
+			)
+			ch <- prometheus.MustNewConstMetric(
+				c.equipmentRunning, prometheus.GaugeValue, is_thing_running(t.ExtendedRuntime.AuxHeat2), append(tFields, "heat 2")...,
+			)
+			ch <- prometheus.MustNewConstMetric(
+				c.equipmentRunning, prometheus.GaugeValue, is_thing_running(t.ExtendedRuntime.AuxHeat3), append(tFields, "heat 3")...,
+			)
+			ch <- prometheus.MustNewConstMetric(
+				c.equipmentRunning, prometheus.GaugeValue, is_thing_running(t.ExtendedRuntime.Cool1), append(tFields, "cooling 1")...,
+			)
+			ch <- prometheus.MustNewConstMetric(
+				c.equipmentRunning, prometheus.GaugeValue, is_thing_running(t.ExtendedRuntime.Cool2), append(tFields, "cooling 2")...,
+			)
+			ch <- prometheus.MustNewConstMetric(
+				c.equipmentRunning, prometheus.GaugeValue, is_thing_running(t.ExtendedRuntime.HeatPump1), append(tFields, "heat pump 1")...,
+			)
+			ch <- prometheus.MustNewConstMetric(
+				c.equipmentRunning, prometheus.GaugeValue, is_thing_running(t.ExtendedRuntime.HeatPump2), append(tFields, "heat pump 2")...,
+			)
+			ch <- prometheus.MustNewConstMetric(
+				c.equipmentRunning, prometheus.GaugeValue, is_thing_running(t.ExtendedRuntime.Fan), append(tFields, "fan")...,
+			)
+
+
+			ch <- prometheus.MustNewConstMetric(
+				c.auxHeat1, prometheus.GaugeValue, is_thing_running(t.ExtendedRuntime.AuxHeat1), tFields...,
+			)
+			ch <- prometheus.MustNewConstMetric(
+				c.auxHeat2, prometheus.GaugeValue, is_thing_running(t.ExtendedRuntime.AuxHeat2), tFields...,
+			)
+			ch <- prometheus.MustNewConstMetric(
+				c.auxHeat3, prometheus.GaugeValue, is_thing_running(t.ExtendedRuntime.AuxHeat3), tFields...,
+			)
+			ch <- prometheus.MustNewConstMetric(
+				c.compCool1, prometheus.GaugeValue, is_thing_running(t.ExtendedRuntime.Cool1), tFields...,
+			)
+			ch <- prometheus.MustNewConstMetric(
+				c.compCool2, prometheus.GaugeValue, is_thing_running(t.ExtendedRuntime.Cool2), tFields...,
+			)
+			ch <- prometheus.MustNewConstMetric(
+				c.heatPump1, prometheus.GaugeValue, is_thing_running(t.ExtendedRuntime.HeatPump1), tFields...,
+			)
+			ch <- prometheus.MustNewConstMetric(
+				c.heatPump2, prometheus.GaugeValue, is_thing_running(t.ExtendedRuntime.HeatPump2), tFields...,
+			)
+			ch <- prometheus.MustNewConstMetric(
+				c.fan, prometheus.GaugeValue, is_thing_running(t.ExtendedRuntime.Fan), tFields...,
 			)
 		}
 		for _, s := range t.RemoteSensors {
@@ -181,6 +334,67 @@ func (c *eCollector) Collect(ch chan<- prometheus.Metric) {
 						)
 					default:
 						log.Errorf("unknown sensor occupancy value %q", sc.Value)
+					}
+				case "vocPPM":
+					if sc.Value == "unknown" {
+						continue
+					}
+					
+					if v, err := strconv.ParseFloat(sc.Value, 64); err == nil {
+						ch <- prometheus.MustNewConstMetric(
+							c.voc, prometheus.GaugeValue, v, sFields...,
+						)
+					} else {
+						log.Errorf("value [%q] was %q", sc.Type, sc.Value)
+						log.Error(err)
+					}
+				case "co2PPM":
+					if sc.Value == "unknown" {
+						continue
+					}
+					if v, err := strconv.ParseFloat(sc.Value, 64); err == nil {
+						ch <- prometheus.MustNewConstMetric(
+							c.co2, prometheus.GaugeValue, v, sFields...,
+						)
+					} else {
+						log.Errorf("value [%q] was %q", sc.Type, sc.Value)
+						log.Error(err)
+					}
+				case "airQualityAccuracy":
+					if sc.Value == "unknown" {
+						continue
+					}
+					if v, err := strconv.ParseFloat(sc.Value, 64); err == nil {
+						ch <- prometheus.MustNewConstMetric(
+							c.air_quality_accuracy, prometheus.GaugeValue, v, sFields...,
+						)
+					} else {
+						log.Errorf("value [%q] was %q", sc.Type, sc.Value)
+						log.Error(err)
+					}
+				case "airQuality":
+					if sc.Value == "unknown" {
+						continue
+					}
+					if v, err := strconv.ParseFloat(sc.Value, 64); err == nil {
+						ch <- prometheus.MustNewConstMetric(
+							c.air_quality, prometheus.GaugeValue, v, sFields...,
+						)
+					} else {
+						log.Errorf("value [%q] was %q", sc.Type, sc.Value)
+						log.Error(err)
+					}
+				case "airPressure":
+					if sc.Value == "unknown" {
+						continue
+					}
+					if v, err := strconv.ParseFloat(sc.Value, 64); err == nil {
+						ch <- prometheus.MustNewConstMetric(
+							c.air_pressure, prometheus.GaugeValue, v, sFields...,
+						)
+					} else {
+						log.Errorf("value [%q] was %q", sc.Type, sc.Value)
+						log.Error(err)
 					}
 				default:
 					log.Infof("ignoring sensor capability %q", sc.Type)
